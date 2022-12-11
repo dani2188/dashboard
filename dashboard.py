b908d6397036751a56c1492bbe98552d5eaebf3f
Lahmer Othmane <106430920+dani2188@users.noreply.github.com>
2022-12-11 17:14:30 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -30,23 +30,31 @@ st.markdown(f"<h7 style='text-align: center;'>{subheader_text}</h7>", unsafe_all
 
 
 # Load test data
-uploaded_file_test = st.file_uploader("Importer un échantillon de clients")
-if uploaded_file_test:
-    X = pd.read_csv(uploaded_file_test)
-    X.set_index('sk_id_curr',inplace=True)
-    st.write(X)
-    # Sélectionner l'id client à partir d'une liste:
-    id_client = st.sidebar.selectbox('Quel user id chosir?', list(map(int, X.index.to_list())))
+X = pd.read_csv('X_test_sample.csv')
+X.set_index('sk_id_curr',inplace=True)
+st.write(X)
+
+# Sélectionner l'id client à partir d'une liste:
+id_client = st.sidebar.selectbox('Quel user id chosir?', list(map(int, X.index.to_list())))
+
     
 #Faire la prédiction et donner sa probabilité
+predict_btn = st.sidebar.button('Donner la prédiction')
+if predict_btn:
+    resultat= requests.post(url='https://myappy.herokuapp.com/predict',json= {'user_id': id_client})
+    
 if uploaded_file_test:
     predict_btn = st.button('Prédire après sélection du user id ')
     if predict_btn:
         if uploaded_file_test:
             resultat= requests.post(url='http://127.0.0.1:8000/predict',json= {'user_id': id_client})
-            st.write(resultat.json()) 
-            #st.write(resultat.json()['prediction'])
-            fig = go.Figure(go.Indicator(mode = "gauge+number",value = resultat.json()['probability'], domain = {'x': [0, 1], 'y': [0, 1]}, title = {'text':    "Probabilité de la prédiction"}))
+            st.write(resultat.json())   
+            #st.write(resultat.json()) 
+            st.write( 'Résultat de la prédiction:', int(resultat.json()['prediction']))
+            # Visualiser la probabilité du résultat sous forme de jauge
+            fig = go.Figure(go.Indicator(mode = "gauge+number",value = round(resultat.json()['probability'],2), domain = {'x': [0, 1], 'y': [0, 1]}, title = {'text':    "Probabilité de la prédiction"},  gauge = {'axis': {'range': [0, 1]}, 
+    'steps' : [{'range': [0, round(resultat.json()['probability'],2)], 'color': "green"}, {'range': [round(resultat.json()['probability'],2), 1], 'color': "red"}]}))
+    
             # Plot
             st.plotly_chart(fig, use_container_width=True)
      
