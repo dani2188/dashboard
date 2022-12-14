bd628e10252973c54972e48d5f8c51ec6ebdf9d6
Lahmer Othmane <106430920+dani2188@users.noreply.github.com>
2022-12-14 09:37:10 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,3 @@
-import plotly
 import streamlit as st
 import requests
 import pandas as pd
@@ -8,21 +7,16 @@ import streamlit.components.v1 as components
 #import dill 
 import numpy as np
 import plotly
-#import plotly.graph_objects as pgo
-#import shap
+import plotly.graph_objects as pgo
+import plotly.express as px
+import shap
+import dill
 #import matplotlib.pyplot as plt
 #import seaborn as sns
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # cd C:/Users/marat/Downloads/Py-DS-ML-Bootcamp-master/OCR/P7/dashboard/
 #streamlit run dashboard.py
-
- 
-# loading the trained model
-#pickle_in = open('lgbm.pkl', 'rb') # quel est le dossier courant?
-#model = pickle.load(pickle_in)
-
-
 
 
 # Titres de l'application
@@ -42,7 +36,7 @@ id_client = st.sidebar.selectbox('Séléctionner un ID client', list(map(int, X.
 
     
 #Faire la prédiction et donner sa probabilité
-predict_btn = st.sidebar.button('Prédire')
+predict_btn = st.sidebar.button('Prédiction')
 if predict_btn:
   resultat= requests.post(url='https://myappy.herokuapp.com/predict',json= {'user_id': id_client})
   #st.write(resultat.json())   
@@ -52,19 +46,8 @@ if predict_btn:
    'steps' : [{'range': [0, round(resultat.json()['probability'],2)], 'color': "green"}, {'range': [round(resultat.json()['probability'],2), 1], 'color': "red"}]}))
   st.plotly_chart(fig, use_container_width=True)
    
-  # plot with seaborn
-  #limits = [0, 1]
-  #palette = sns.color_palette("coolwarm_r", len(limits))
-  #fig, ax = plt.subplots()
-  #ax.set_aspect('equal')
-  #ax.set_yticks([1])
-  #ax.set_yticklabels(['Probabilité du résultat']) 
-  # Draw the value we're measuring
-  #ax.barh([1], round(resultat.json()['probability'],2), color='black', height=5)
-  # Plot
-  #st.plotly_chart(fig, use_container_width=True)
-     
-    
+ 
+       
 
 # Définition d'une fonction pour visualisation shap
 def st_shap(plot, height=None):
@@ -72,21 +55,47 @@ def st_shap(plot, height=None):
     components.html(shap_html, height=height)
     
 #Interprétabilité des résultats avec SHAP:   
-predict_btn_res = st.sidebar.button("Expliquer les résultats")
+predict_btn_res = st.sidebar.button("Analyse de la prédiction")
 if predict_btn_res:
   #data_in = X.loc[[id_client]]
-  # explain the model's predictions using SHAP         
-  explainer = dill.load('explainer.pkl')
-  #explain the model's predictions using SHAP values
-  shap_values = dill.load('shap_values.pkl')    
+  
+  # explain the model's predictions using SHAP
+  # python v7 required for dill (in streamlitshare)
+  # Loading explainer
+  explainer_file = open("explainer.pkl", "rb")
+  explainer = dill.load(explainer_file)
+  explainer_file.close()
+  
+  #Loading shap_values
+  shap_values_file = open("shap_values.pkl", "rb")
+  shap_values = dill.load(shap_values_file)
+  shap_values_file.close()   
+
   # visualize the prediction's explanation:
-  st_shap(shap.force_plot(explainer.expected_value[1], shap_values[1][0,:], X.loc[[id_client]]), 200)
+  st_shap(shap.force_plot(explainer.expected_value, shap_values[1][0,:], X.loc[[id_client]]), 200)
   # ALL prédictions
-  st_shap(shap.force_plot(explainer.expected_value[1], shap_values[1], X),400)
+  st_shap(shap.force_plot(explainer.expected_value, shap_values[1], X),400)
   st.pyplot(shap.summary_plot(shap_values, X))
             
             
-            
+  
+# Distribition des top features importance:
+dist_btn_res = st.sidebar.button("Positionnement du client (Top Features)")
+if dist_btn_res:
+  # Feature: code_gender
+  st.write('{code_gender} du client: ', X.loc[[id_client]]['code_gender'].values[0])
+  fig= px.histogram(X, x="code_gender", color="code_gender")
+  st.plotly_chart(fig)
+  # Feature: payment_rate
+  st.write('{payment_rate} du client: ', X.loc[[id_client]]['payment_rate'].values[0])
+  fig= px.box(X, y="payment_rate")
+  st.plotly_chart(fig)
+  # Feature: region_rating_client_w_city
+  st.write('{region_rating_client_w_city} du client: ', X.loc[[id_client]]['region_rating_client_w_city'].values[0])
+  fig= px.histogram(X, x="region_rating_client_w_city")
+  st.plotly_chart(fig)
+
+  
             
             
     
